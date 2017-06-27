88c3143eba850f1501a8e61ad74c6bcf0fdb0517
Olli-Pekka Oras <Olli-Pekka.Oras@conmio.com>
2017-06-27 15:39:56 +0300

    Added tests for DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -101,3 +101,41 @@ describe('GET /todos/:id', () => {
             .end(done);
     })
 })
+
+describe('DELETE /todos/:id', () => {
+    it('should remote a todo', (done) => {
+        var hexid = todos[1]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexid}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexid);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+
+                Todo.findById(hexid).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e) => done(e));
+            });
+    })
+
+    it('should return 404 if todo not found', (done) => {
+        var hexID = new ObjectID().toHexString();
+
+        request(app)
+            .delete(`/todos/${hexID}`)
+            .expect(404)
+            .end(done);
+    })
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+            .delete(`/todos/123`)
+            .expect(404)
+            .end(done);
+    })
+})
